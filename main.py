b84e2f0cb6dd24ce12fed9447ca6675851f49cb6
David Siddle <ccaedsi@ucl.ac.uk>
2023-04-13 14:58:11 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+from selenium import webdriver
+import time
+from selenium.webdriver.chrome.service import Service
+
+PATH = "C:\\Users\\ccaedsi\\Selenium\\chromedriver.exe"
+service = Service(PATH)
+driver = webdriver.Chrome(service=service)
+
+driver.get("https://davd-siddle-portfolio.netlify.app/")
+
+time.sleep(10) # wait for 10 seconds before closing the browser
+print(driver.title)
+driver.quit() # close the browser
